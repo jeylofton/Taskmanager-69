e248e6d7872a1a3d14d4cab4c1a0c89379a39575
jeylofton <jeylofton@gmail.com>
2026-07-22 20:37:05 -0500

    Break Updated Code

--- scripts/app.js
+++ scripts/app.js
@@ -13,10 +13,39 @@ const budget = $("#numBudget").val();
     console.log(taskToSave);
 }
 
+const API = "https://106api-b0bnggbsgnezbzcz.westus3-01.azurewebsites.net/api/tasks";
 
+function loadTask(){
+    $.ajax({
+        type:"get", //HTTP Verb. READ
+        url: API,
+        success: function(data){
+            console.log(data)
+        },
+        error:function(err){
+            console.log(err);
+        }
+        
+    })
+}
 
-
-
+function displayTask(task){
+    let syntax =  `
+    <div class="task" style="border-color:${task.color}">
+    <div class="info">
+        <h4>${task.title}</h4>
+        <p>${task.desc}</p>
+    </div>
+    <label class="status">${task.status}</label>
+    <div class="date-budget">
+        <label>Due: ${task.date}</label>
+        <label>Budget: $${task.budget}</label>
+    </div>
+    </div>`;
+    
+  // Inject the new HTML into the DOM Tree
+$(".list").append(syntax);
+}
 
 function init(){
     // console.log("Hello from 106");
@@ -26,7 +55,6 @@ function init(){
 function second(){
     // console.log("Hello There");
 }
-
 
 window.onload = init;
 // It force that the HTML and the CSS gets resolved before that the logic gets executed.
